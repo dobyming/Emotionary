025805c1a3279e08ebe5914faa0f86d47bd99807
dobyming <12184874@inha.edu>
2023-02-16 15:41:27 +0900

    Learn Route Library Concepts

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,24 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+    const navigate = useNavigate();
+    const[searchParams,setSearchParams] = useSearchParams();
+    const id = searchParams.get('id');
+    const mode = searchParams.get('mode');
+    console.log(id);
+    console.log(mode);
+
     return(
         <div>
             <h1>Edit</h1>
             <p>이곳은 Edit.</p>
+            <button onClick={()=>{setSearchParams({who:'damin'})}}>QS 바꾸기</button>
+            <button onClick={()=>{
+                navigate('/home');
+            }}>Home으로 가기</button>
+            <button onClick={()=>{
+                navigate(-1)
+            }}>뒤로 가기</button>
         </div>
     );
 };
