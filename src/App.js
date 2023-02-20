c8ec404ab6aaaee0c6be347810a5138d68790846
dobyming <12184874@inha.edu>
2023-02-20 16:46:13 +0900

    Manage state function with useReducer and setting Provider for state functions

--- src/App.js
+++ src/App.js
@@ -1,47 +1,93 @@
+import React, { useReducer,useRef } from 'react';
 import './App.css';
-import { BrowserRouter, Routes , Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Edit from './pages/Edit';
 import Diary from './pages/Diary';
 import New from './pages/New';
 
-//COMPONENTS
-import MyButton from './components/MyButton';
-import MyHeader from './components/MyHeader';
+const reducer = (state, action) => {
+  let newState = [];
+  switch (action.type) {
+    case 'INIT': {
+      return action.data;
+    }
+    case 'CREATE': {
+      const newItem = {
+        ...action.data,
+      };
+      newState = [newItem, ...state];
+      break;
+    }
+    case 'REMOVE': {
+      newState = state.filter((it) => it.id !== action.targetId);
+      break;
+    }
+    case 'EDIT': {
+      newState = state.map((it) => (it.id === action.data.id ? { ...action.data } : it));
+      break;
+    }
+    default:
+      return state;
+  }
+  return newState;
+};
+
+export const DiaryStateContext = React.createContext();
+export const DiaryDispatchContext = React.createContext();
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <MyHeader 
-        headText={"APP"} 
-        leftChild={<MyButton text={"왼쪽버튼"} onClick={()=>{alert("왼쪽")}}/>}
-        rightChild={<MyButton text={"오른쪽버튼"} onClick={()=>alert('오른쪽 버튼')}/>}
-        />
-        
-        <h2>App.js</h2>
-        <MyButton 
-        text={"버튼"} 
-        onClick={()=>alert("버튼 클릭")} 
-        type={"POSITIVE"}/>   
-        
-        <MyButton 
-        text={"버튼"} 
-        onClick={()=>alert("삭제")} 
-        type={"NEGATIVE"}/>        
-        
-        <MyButton 
-        text={"버튼"} 
-        onClick={()=>alert("수정")}/> 
+  const [data, dispatch] = useReducer(reducer, []); // 상태관리
+  
+  const dataId = useRef(0);
+  // CREATE
+  const onCreate = (date,content,emotion) => {
+    dispatch({
+      type:'CREATE',
+      data: {
+        id: dataId.current,
+        date: new Date(date).getTime(),
+        content,
+        emotion
+      }
+    });
+    dataId.current += 1;
+  };
 
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/new' element={<New/>}/>
-          <Route path='/edit' element={<Edit/>}/>
-          <Route path='/diary/:id' element={<Diary/>}/>
-        </Routes>
-      </div>
-    </BrowserRouter>
+  // REMOVE 
+  const onRemove = (targetId) => {
+    dispatch({type:'REMOVE', targetId});
+  };
+
+  // EDIT
+  const onEdit = (targetId,date,content,emotion) => {
+    dispatch({
+      type:'EDIT',
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotion
+      }
+    });
+  };
+
+  return (
+    <DiaryStateContext.Provider value = {data}>
+      <DiaryDispatchContext.Provider value={{onCreate,onEdit,onRemove}}>
+        <BrowserRouter>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/new" element={<New />} />
+              <Route path="/edit" element={<Edit />} />
+              <Route path="/diary/:id" element={<Diary />} 
+              />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </DiaryDispatchContext.Provider>
+    </DiaryStateContext.Provider>
   );
 }
 
