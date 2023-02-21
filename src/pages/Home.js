d6e7e92faf2344f82f5fec55014574710ef21795
dobyming <12184874@inha.edu>
2023-02-21 15:49:28 +0900

    Implementation of rendering diaryList by validating Date

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,37 @@
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { DiaryStateContext } from "../App";
+
 import MyHeader from './../components/MyHeader';
 import MyButton from './../components/MyButton';
+import DiaryList from "../components/DiaryList";
 
 const Home = () => {
+    const diaryList = useContext(DiaryStateContext); //일기 Data 담김
+    
+    const [data,setData] = useState([]);
     const [curDate,setCurDate] = useState(new Date());
     const headText = `${curDate.getFullYear()}년 ${curDate.getMonth()+1}월`
     
+    //월초~월말 사이에 있는지 validate
+    useEffect(()=>{
+        if (diaryList.length >=1){
+            const initDay = new Date(
+                curDate.getFullYear(),
+                curDate.getMonth(),
+                1
+            ).getTime(); //월초
+            
+            const lastDay = new Date(
+                curDate.getFullYear(),
+                curDate.getMonth()+1,
+                0
+            ).getTime(); //월말
+
+            setData(diaryList.filter((it)=> initDay <= it.date && it.date <= lastDay));
+        }
+    },[diaryList,curDate]);
+
+    // header 날짜 show 
     const increaseMonth = () => {
         setCurDate(
             new Date(curDate.getFullYear(), curDate.getMonth()+1 , curDate.getDate())
@@ -24,6 +50,7 @@ const Home = () => {
             leftChild = {<MyButton text = {'<'} onClick={()=>decreaseMonth()}/>}
             rightChild = {<MyButton text = {'>'} onClick={()=>increaseMonth()}/>}
             />
+            <DiaryList diaryList={data}/>
         </div>
     );
 };
