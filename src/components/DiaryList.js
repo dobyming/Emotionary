461056e34a3d86bef99ee73e3f811feea6af6dff
dobyming <12184874@inha.edu>
2023-02-22 14:00:18 +0900

    Implement filter by emotion scores

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -5,6 +5,12 @@ const sortOptionList = [
     {value:'oldest', name: '오래된순'}
 ];
 
+const filterOptionList = [
+    {value:"all", name: "전부다" },
+    {value:"good", name: "좋은 감정만"},
+    {value:"bad", name: "안좋은 감정만"}
+];
+
 const ControlMenu = ({value,onChange,optionList}) => {
     return (
         <select value={value} onChange = {(e)=>onChange(e.target.value)}>
@@ -17,8 +23,17 @@ const ControlMenu = ({value,onChange,optionList}) => {
 
 const DiaryList = ({diaryList}) => {
     const [sortType,setSortType] = useState('latest');
-
-    const showByTimeDiary = () => {
+    const [filter, setFilter] = useState('all');
+    //감정 점수에 따른 정렬
+    const getProcessedList = () => {
+        const filterCallBack = (item) => {
+            if (filter === 'good') {
+                return parseInt(item.emotion) <=3;
+            } else {
+                return parseInt(item.emotion) > 3;
+            }
+        }
+        //시간 순 정렬
         const compare = (a,b) => {
             if (sortType === 'latest') {
                 return parseInt(b.date) - parseInt(a.date)
@@ -26,20 +41,28 @@ const DiaryList = ({diaryList}) => {
                 return parseInt(a.date) - parseInt(b.date)
             }
         }
+        
         const copyList = JSON.parse(JSON.stringify(diaryList));
-        const sortedList = copyList.sort(compare);
+        const filteredList = filter === 'all' ? copyList : copyList.filter((it)=>filterCallBack(it));
+        const sortedList = filteredList.sort(compare);
+        
         return sortedList;
     };
 
     return (
         <div>
             <ControlMenu 
-            value ={sortType} 
-            onChange = {setSortType}
-            optionList = {sortOptionList}
+             value ={sortType} 
+             onChange = {setSortType}
+             optionList = {sortOptionList}
             />
-            {showByTimeDiary().map((it)=>
-            <div key={it.id}>{it.content}</div>
+            <ControlMenu
+             value = {filter}
+             onChange = {setFilter}
+             optionList = {filterOptionList}
+            />
+            {getProcessedList().map((it)=>
+            <div key={it.id}>{it.content} {it.emotion}</div>
             )}
         </div>
     );
